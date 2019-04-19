468ba745d71d044f9eaf62755afb638f8e5af9ec
Borisz <borisz.voros@cheppers.com>
2019-04-19 13:56:22 +0200

    theming

--- sites/all/modules/motor/motor_news.tpl.php
+++ sites/all/modules/motor/motor_news.tpl.php
@@ -1,9 +1,9 @@
 <div class="news">
-	<div><img src=" <?php print (string)$article->enclosure['url'][0]; ?>" width="300"></div>
+	<div class="news-image"><img src=" <?php print (string)$article->enclosure['url'][0]; ?>" width="300"></div>
 <?php
 print '<span class="news-pubdate">' . (string)$article->pubDate . '</span>';
 print '<h2 class="news-title">' . (string)$article->title . '</h2>';
 print '<p class="news-description">' . (string)$article->description . '</p>';
-print (string)$article->category;
+print '<div class="news-tags">' . (string)$article->category . '</div>';
 ?>
 </div>
